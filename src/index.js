38dd704a1e2f57bcabde6c8a1069a76f20bddd87
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-10-31 08:08:53 +0100

    Final commit. Insert two html elements wraped in a div into a js file.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World!</h1>
+        <p>This is a tiny paragraph.</p>
+    </div>
+    , document.getElementById("root"));
